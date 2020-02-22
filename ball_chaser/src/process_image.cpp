0a2de6d6054e58f4c4dc03701f2ea73ac5d700b9
Jus <juspch@gmail.com>
2020-02-22 17:16:34 +0700

    type: code change changed process_image.cpp to properly assess white pixels

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -82,8 +82,8 @@ void process_image_callback(const sensor_msgs::Image img)
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
 
-    for (int i = 0; i < img.height * img.step; i++) {
-        if (img.data[i] == white_pixel) {
+    for (int i = 0; i < img.height * img.step; i+=3) {
+        if (img.data[i] == white_pixel && img.data[i+1] == white_pixel && img.data[i+2] == white_pixel) {
             if (i % img.step < img.step / 3 && i % img.step >= 0 ) {
               left++;
             }
